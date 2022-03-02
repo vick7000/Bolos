c2888fb8b91d31e224d014f0ef4e882f8de420da
bellimatheus <85229965+bellimatheus@users.noreply.github.com>
2022-03-02 08:00:26 -0300

    OutraParte

---
+++ frontendRestante/usuario/script.js
@@ -0,0 +1,20 @@
+// Alterar
+var nome = document.getElementById("nome");
+var cadEmail = document.getElementById("cadEmail");
+var cadSenha = document.getElementById("senha");
+var rua = document.getElementById("rua");
+var numero = document.getElementById("num");
+var bairro = document.getElementById("bairro");
+var cep = document.getElementById("cep");
+var complemento = document.getElementById("comp"); //nao obrigatório
+var nomeTitular = document.getElementById("titular");
+var numeroCartao = document.getElementById("numero");
+var dataEmissao = document.getElementById("emissao");
+var validade = document.getElementById("validade");
+var codigo = document.getElementById("cod");
+var pix = document.getElementById("pix");
+
+
+function alterando(){
+
+}
