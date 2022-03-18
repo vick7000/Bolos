746eb149ab5add9aba8716b23b8d2754d79df369
Belli <bellimatheus3@gmail.com>
2022-03-18 13:48:06 -0300

    arrumando

---
+++ back3nd/src/controller/ItemController.js
@@ -0,0 +1,69 @@
+const Item = require('../model/item');
+const Config = require('../model/config');
+
+
+const create = async (req, res) => {
+    const data = req.body;
+
+    const ret = await Item.create(data);
+
+    res.json(ret);
+}
+
+const read = async (req, res) => {
+    const id = req.params.id;
+
+    let id_config = req.query.id_config;
+
+    let filtro = {};
+
+    if(id !== undefined) filtro = {where: {id: id}};
+
+    filtro.include = {
+        exclude: ['id_config']
+    }
+
+    filtro.include = [
+        { model: Config}
+    ];
+
+    if(id_config !== undefined){
+        filtro.include[0].where = {id: id_config}
+    }
+    const ret = await Item.findAll(filtro);
+
+    res.json(ret);
+}
+
+const update = async (req, res) => {
+    const id = req.params.id;
+
+    const data = req.body;
+
+    let ret = await Item.update(data, {where:{id: id}});
+
+    ret = await Item.findAll({where: {id: id}});
+
+    res.json(ret);
+}
+
+const remove = async (req, res) => {
+    const id = req.params.id;
+
+    const ret = await Item.destroy({where: {id: id}});
+
+    if(ret == 1) {
+        res.json({id: id})
+    }else {
+        res.status(400).send();
+    }
+
+
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    remove,
+}
---
+++ back3nd/src/controller/UsuarioController.js
@@ -0,0 +1,93 @@
+const Usuario = require('../model/usuario');
+
+const create = async (req, res) => {
+    const data = req.body;
+
+    let ret = [];
+
+    try {
+        ret = await Usuario.create(data);
+
+        delete ret.dataValues.senha;
+    }catch(err) {
+        if(err.parent.code == 'ER_DUP_ENTRY') {
+            ret = {
+                msg: 'Email já cadastrado'
+            }
+            res.status(400);
+        }
+    }
+
+    res.json(ret);
+}
+
+const read = async (req, res) => {
+    let filtro = {};
+    
+    let id = req.params.id;
+
+    if(id != undefined) filtro = { where : { id: id }}
+
+    const ret = await Usuario.findAll(filtro);
+
+    res.json(ret);
+}
+
+const update = async (req, res) => {
+    const id = req.params.id;
+
+    const data = req.body;
+
+    let ret = await Usuario.update(data, {
+        where : { id: id}
+    });
+
+    ret = await Usuario.findAll({ 
+        attributes: {
+            exclude: ['senha']
+        },
+        where : {id: id}
+    })
+
+    res.json(ret);
+}
+
+const remove = async (req, res) => {
+    const id = req.params.id;
+
+    const ret = await Usuario.destroy({
+        where: {id: id}
+    })
+    
+    if(ret == 1) {
+        res.json({id: id});
+    }else {
+        res.status(400).send();
+    }
+}
+
+const login = async (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+
+    const ret = await Usuario.findAll({
+        attributes: {
+            exclude: ['senha']
+        },
+        where: {
+            email: data.email,
+            senha: data.senha,
+        }
+    });
+
+    res.json(ret);
+}
+
+module.exports = {
+    create,
+    read,
+    update,
+    remove,
+    login,
+}
