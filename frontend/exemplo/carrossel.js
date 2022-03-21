8bb844461033a0f6073cb7e3ddbcb23122ae7599
Victória Rosa <vickrosa7@hotmail.com>
2022-03-21 11:27:13 -0300

    front

--- frontend/exemplo/carrossel.js
+++ frontend/exemplo/carrossel.js
@@ -9,6 +9,8 @@ function toggleState3() {
   if (slider.classList.contains("active")) {
     slider.classList.remove("active");
     outer.classList.remove("outerActive");
+    galleryView.style.display = "flex";
+    tilesView.style.display = "none";
     
     
     while (tilesContainer.hasChildNodes()) {
@@ -17,7 +19,8 @@ function toggleState3() {
   } else {
     slider.classList.add("active");
     outer.classList.add("outerActive");
-    
+    galleryView.style.display = "none";
+    tilesView.style.display = "flex"
      
     for (let i = 0; i < imgObject.length - 1; i++) {
       let tileItem = document.createElement("div");
@@ -32,31 +35,15 @@ function toggleState3() {
 let imgObject = [
   "https://cdn.folhape.com.br/img/pc/1100/1/dn_arquivo/2021/10/0910sa2120.jpg",
   "../assets/pedido.png",
-  "https://placeimg.com/450/450/architecture",
-  "https://placeimg.com/450/450/nature",
-  "https://placeimg.com/450/450/people",
-  "https://placeimg.com/450/450/tech",
-  "https://picsum.photos/id/1/450/450",
-  "https://picsum.photos/id/8/450/450",
-  "https://picsum.photos/id/12/450/450",
-  "https://picsum.photos/id/15/450/450",
+  "https://www.oetker.com.br/Recipe/Recipes/oetker.com.br/br-pt/baking/image-thumb__70184__RecipeDetailsLightBox/bolo-com-recheio-de-leite-em-po.jpg",
+  "https://media.gazetadopovo.com.br/2022/03/01091229/bento-cake-960x540.jpeg",
+  "https://www.tuacasa.com.br/wp-content/uploads/2020/11/bolo-aniversario-masculino-00.png",
+  "https://i.pinimg.com/736x/56/28/e1/5628e1fd77a1b57ee06676e5d6a55bb5.jpg",
+  "https://www.confeitariaxv.com.br/wp-content/uploads/sites/251/2016/07/IMG_3014.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfEQ2okUDfIyArfcJj3PghqyBCn6eQgU2EGQ&usqp=CAU",
+  "https://img.elo7.com.br/product/zoom/2B5B92B/bolos-decorados-chocolate.jpg",
+  "https://coolinary.com.br/wp-content/uploads/2020/09/Bolo-ilha-gelatina-8.jpg",
 ];
-
-let testeObject = [
-  "Um belissimo bolo cortado",
-  "../assets/pedido.png",
-  "https://placeimg.com/450/450/architecture",
-  "https://placeimg.com/450/450/nature",
-  "https://placeimg.com/450/450/people",
-  "https://placeimg.com/450/450/tech",
-  "https://picsum.photos/id/1/450/450",
-  "https://picsum.photos/id/8/450/450",
-  "https://picsum.photos/id/12/450/450",
-  "https://picsum.photos/id/15/450/450",
-  "https://picsum.photos/id/5/450/450",
-];
-
-
 
   let textObject =[
     {
@@ -127,8 +114,6 @@ let testeObject = [
     },
   ]
     
-
-
 let mainImg = 0;
 let prevImg = imgObject.length - 1;
 let nextImg = 1;
@@ -141,7 +126,7 @@ function loadGallery() {
 
   let mainView = document.getElementById("mainView");
   mainView.style.background = "url(" + imgObject[mainImg] + ")";
-  document.getElementById("texto").innerHTML = "forma : " + textObject[mainImg].forma + "<br>" + "Recheio : " + textObject[mainImg].recheio + "<br>" + "Cobertura : " + textObject[mainImg].cobertura + "<br>" + "Preço : " + textObject[nextImg].valor;
+  document.getElementById("texto").innerHTML = "Forma : " + textObject[mainText].forma + "<br>" + "<br>" + "Recheio : " + textObject[mainImg].recheio + "<br>" + "<br>" + "Cobertura : " + textObject[mainImg].cobertura + "<br>" + "<br>" + "Preço : " + textObject[nextImg].valor;
   mainView.style.backgroundSize = "cover";
 
   for (i = 0, i < textObject.length - 1; i++;) {
@@ -165,7 +150,7 @@ function scrollRight() {
 
   prevImg = mainImg;
   mainImg = nextImg;
-  if (nextImg >= (imgObject.length -1) && nextText >= (textObject.length -1)) {
+  if (nextImg >= (imgObject.length -1)) {
     nextImg = 0;
     nextText = 0;
   } else {
@@ -182,9 +167,10 @@ function scrollLeft() {
   nextImg = mainImg;
   mainImg = prevImg;
    
-  if (prevImg === 0 && prevText) {
+  if (prevImg === 0 && prevText === 0) {
     prevImg = imgObject.length - 1;
     prevText = textObject.length - 1;
+    
   } else {
     prevImg--;
     prevText--;
@@ -205,6 +191,3 @@ document.addEventListener('keyup',function(e){
 });
 
 loadGallery();
-
-
-
