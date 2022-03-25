8638d8d3178cab37045bda08b0baccfe997cc785
bellimatheus <bellimatheus3@gmail.com>
2022-03-25 10:15:16 -0300

    bacc

--- Backend/src/controller/UsuarioController.js
+++ Backend/src/controller/UsuarioController.js
@@ -22,11 +22,11 @@ const create = async (req, res) => {
 }
 
 const read = async (req, res) => {
+    const id = req.params.id;
+
     let filtro = {};
     
-    let id = req.params.id;
-
-    if(id != undefined) filtro = { where : { id: id }}
+    if(id != undefined) filtro = { where: { id: id }}
 
     const ret = await Usuario.findAll(filtro);
 
