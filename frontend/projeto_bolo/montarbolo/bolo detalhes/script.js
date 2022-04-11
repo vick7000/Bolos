415104694c0388e41a1eef98e4d72568a4224364
vick7000 <gabrielalexsandro2020@gmail.com>
2022-04-11 09:22:58 -0300

    porim

--- frontend/projeto_bolo/montarbolo/bolo detalhes/script.js
+++ frontend/projeto_bolo/montarbolo/bolo detalhes/script.js
@@ -6,10 +6,10 @@ $('#service').on('change', function () {
     .val()
 
   if (value == 1) {
-    $('#valor').html('<model-viewer src="assets/formato/bolocilindro.glb" camera-controls auto-rotate ar ios-src="assets/formato/bolocilindro.glb" /> <p><a href="./abolo.html">Escolher recheio</a></p> ')
+    $('#valor').html('<model-viewer src="assets/formato/bolocilindro.glb" camera-controls auto-rotate ar ios-src="assets/formato/bolocilindro.glb" /> <p><a href="./abolo.html">Escolher recheio<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p> ')
 
   } else if (value == 10) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo rosa mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo rosa brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo rosa mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo rosa brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 60
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -18,7 +18,7 @@ $('#service').on('change', function () {
       
     }) 
   } else if (value == 16) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo rosa brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo rosa brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo rosa brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo rosa brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 59
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -28,7 +28,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 11) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo azul mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo azul mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo azul mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo azul mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 58
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -38,7 +38,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 17) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo azul brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo azul brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo azul brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo azul brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 57
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -48,7 +48,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 12) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo verao mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo verao mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo verao mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo verao mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 52
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -58,7 +58,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 18) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo verao brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo verao brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo verao brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo verao brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 51
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -68,7 +68,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 13) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo floresta mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo floresta mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo floresta mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo floresta mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 54
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -78,7 +78,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 19) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo floresta brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo floresta brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo floresta brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo floresta brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 53
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -88,7 +88,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 14) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo ciasto mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo ciasto mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo ciasto mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo ciasto mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 56
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -98,7 +98,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 20) {
-    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo ciasto brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo ciasto brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/boloretangulo ciasto brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/boloretangulo ciasto brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 55
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -111,11 +111,11 @@ $('#service').on('change', function () {
   
   //CILINDRO
   else if (value == 21) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro rosa mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro neon rosa mirtiloo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro rosa mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro neon rosa mirtiloo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
   
   
   } else if (value == 22) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro rosa brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro rosa brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro rosa brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro rosa brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 9
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -125,7 +125,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 23) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro neon azul mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro neon azul mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro neon azul mirtilo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro neon azul mirtilo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 8
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -135,7 +135,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 24) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro azul brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro azul brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro azul brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro azul brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 7
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -145,7 +145,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 25) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro verao mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro verao mirtiloo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro verao mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro verao mirtiloo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 2
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -155,7 +155,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 26) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro verao brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro verao brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro verao brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro verao brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 1
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -165,7 +165,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 27) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro neon floresta mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro neon floresta mirtiloo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro neon floresta mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro neon floresta mirtiloo.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 4
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -175,7 +175,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 28) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro floresta brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro floresta brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro floresta brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro floresta brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 3
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -185,7 +185,7 @@ $('#service').on('change', function () {
     })
 
   } else if (value == 29) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro ciasto mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro ciasto brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro ciasto mirtiloo.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro ciasto brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 6
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
@@ -195,7 +195,7 @@ $('#service').on('change', function () {
     })
   
   } else if (value == 30) {
-    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro ciasto brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro ciasto brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home</a></p>')
+    $('#valor').html('<model-viewer src="assets/enfeite/bolocilindro ciasto brigadeiro.glb" camera-controls auto-rotate ar ios-src="assets/enfeite/bolocilindro ciasto brigadeiro.glb"/> <p><a href="../../../home/index.html">Enviar bolo para o carrinho e voltar para home<img src="./png/bx-badge-check.png" width="70" height="70"/><img src="./png/bx-cart-add.png" width="70" height="70"/></a></p>')
     id_config = 5
     fetch("http://localhost:5000/config/" + id_config)
     .then(res => { return res.json() })
